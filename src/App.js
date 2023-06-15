7bf09156e4d8ee2b90c2747ded7b507189981c81
Yoba Fett <alexka.680@gmail.com>
2023-06-15 20:55:48 +0900

    Get posts

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
+import { useState } from 'react';
+import { useEffect } from 'react';
 
 function App() {
+  const [list, setList] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:5000/api/posts')
+      .then(function (response) {
+        console.log(response.data);
+        setList(response.data);
+      })
+      .catch(function (error) {
+        console.log(error);
+      })
+      .finally(function () {
+      });
+  }, []);
+
+  const listContent = list.map(item => {
+    return (
+      <li key={item.id}>
+        author_id: {item.author_id}<br/>
+        created: {item.created}<br/>
+        description: {item.description}<br/>
+        text: {item.text}<br/>
+        title: {item.title}<br/>
+        updated: {item.updated}<br/>
+      </li>
+    )
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='ItemsList'>
+        <ul>{listContent}</ul>
+      </div>
     </div>
   );
 }
